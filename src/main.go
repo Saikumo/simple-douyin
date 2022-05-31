08ff6cf24e8b2406ce8c52807be79c1c438ac2c4
Xufuhao <saikumo11@gmail.com>
2022-05-31 21:03:04 +0800

    添加配置文件

--- src/main.go
+++ src/main.go
@@ -1,1 +1,12 @@
-package src
+package main
+
+import (
+	"fmt"
+	. "saikumo.org/simple-douyin/src/config"
+	"strconv"
+)
+
+func main() {
+	addr := strconv.Itoa(Config.GetInt("server.port"))
+	fmt.Println(addr)
+}
