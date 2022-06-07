af5d5b428b799140b167486c3c197bd90e64fcaa
Xufuhao <saikumo11@gmail.com>
2022-06-07 09:44:31 +0800

    添加错误、校验常量

---
+++ src/enum/error.go
@@ -0,0 +1,8 @@
+package enum
+
+const (
+	USERNAME_IS_NULL          string = "用户名不能为空"
+	USERNAME_OVER_MAX_LENGTH  string = "用户名长度不能超过32个字符"
+	PASSWORD_IS_NULL          string = "密码不能为空"
+	PASSWORD_LENGTH_NOT_VALID string = "密码长度必须为6到32个字符"
+)
---
+++ src/enum/validation.go
@@ -0,0 +1,7 @@
+package enum
+
+const (
+	MAX_USERNAME_LENGTH int = 32
+	MIN_PASSWORD_LENGTH int = 6
+	MAX_PASSWORD_LENGTH int = 32
+)
